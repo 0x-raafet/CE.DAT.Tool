874259140d3c9680dbe5666b728b3fd1df1779b6
GHOST ECHO <109506437+0x-raafet@users.noreply.github.com>
2024-05-18 12:30:15 +0100

    Add files via upload

--- CE.Unpacker/CE.Unpacker/Program.cs
+++ CE.Unpacker/CE.Unpacker/Program.cs
@@ -8,35 +8,51 @@ namespace CE.Unpacker
         static void Main(String[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Conflict Engine DAT Unpacker");
+            Console.WriteLine("Conflict Engine DAT Tool");
             Console.WriteLine("(c) 2021 Ekey (h4x0r) / v{0}\n", Utils.iGetApplicationVersion());
             Console.ResetColor();
 
-            if (args.Length != 2)
+            if (args.Length < 3 || (args[0] != "unpack" && args[0] != "pack"))
             {
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("[Usage]");
-                Console.WriteLine("    CE.Unpacker <m_File> <m_Directory>\n");
-                Console.WriteLine("    m_File - Source of DAT archive file");
-                Console.WriteLine("    m_Directory - Destination directory\n");
+                Console.WriteLine("    CE.Unpacker unpack <m_File> <m_Directory>");
+                Console.WriteLine("    CE.Unpacker pack <m_Directory> <m_File>\n");
+                Console.WriteLine("    m_File - Source or destination of DAT archive file");
+                Console.WriteLine("    m_Directory - Source or destination directory\n");
                 Console.ResetColor();
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("[Examples]");
-                Console.WriteLine("    CE.Unpacker E:\\Games\\CDS\\frontend.pc.dat D:\\Unpacked");
+                Console.WriteLine("    CE.Unpacker unpack E:\\Games\\CDS\\frontend.pc.dat D:\\Unpacked");
+                Console.WriteLine("    CE.Unpacker pack D:\\Unpacked E:\\Games\\CDS\\frontend.pc.dat");
                 Console.ResetColor();
                 return;
             }
 
-            String m_DatFile = args[0];
-            String m_Output = Utils.iCheckArgumentsPath(args[1]);
+            String m_Mode = args[0];
+            String m_FileOrDir = args[1];
+            String m_Output = Utils.iCheckArgumentsPath(args[2]);
 
-            if (!File.Exists(m_DatFile))
+            if (m_Mode == "unpack")
             {
-                Utils.iSetError("[ERROR]: Input DAT file -> " + m_DatFile + " <- does not exist");
-                return;
-            }
+                if (!File.Exists(m_FileOrDir))
+                {
+                    Utils.iSetError("[ERROR]: Input DAT file -> " + m_FileOrDir + " <- does not exist");
+                    return;
+                }
 
-            DatUnpack.iDoIt(m_DatFile, m_Output);
+                DatUnpack.iDoIt(m_FileOrDir, m_Output);
+            }
+            else if (m_Mode == "pack")
+            {
+                if (!Directory.Exists(m_FileOrDir))
+                {
+                    Utils.iSetError("[ERROR]: Input directory -> " + m_FileOrDir + " <- does not exist");
+                    return;
+                }
+
+                DatPack.iDoIt(m_FileOrDir, m_Output);
+            }
         }
     }
 }
