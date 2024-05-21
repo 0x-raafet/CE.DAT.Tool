fdfbad4e2c1633af2e63a52877b38e1a85a69c3f
GHOST ECHO <109506437+0x-raafet@users.noreply.github.com>
2024-05-21 11:06:15 +0100

    Add files via upload

--- CE.Unpacker/CE.Unpacker/FileSystem/Package/DatPack.cs
+++ CE.Unpacker/CE.Unpacker/FileSystem/Package/DatPack.cs
@@ -10,38 +10,59 @@ namespace CE.Unpacker
 
         public static void iDoIt(String m_Directory, String m_OutputFile)
         {
-            DatHashList.iLoadProject();
+            //DatHashList.iLoadProject();
 
             using (BinaryWriter TDatStream = new BinaryWriter(File.Create(m_OutputFile)))
             {
                 var files = Directory.GetFiles(m_Directory, "*.*", SearchOption.AllDirectories);
 
+                Byte[] lpReserved = new Byte[files.Length * 12]; // or 32768 bytes
+                TDatStream.Write(lpReserved);
+
                 foreach (var file in files)
                 {
-                    byte[] fileData = File.ReadAllBytes(file);
-                    UInt32 dwHash = DatHash.iGetHash(Path.GetFileName(file));
-                    UInt32 dwOffset = (UInt32)TDatStream.BaseStream.Position;
-                    Int32 dwSize = fileData.Length;
+                    var TEntry = new DatEntry();
 
-                    var TEntry = new DatEntry
+                    byte[] fileData = File.ReadAllBytes(file);
+
+                    if (!file.Contains("__Unknown"))
                     {
-                        dwHash = dwHash,
-                        dwOffset = dwOffset,
-                        dwSize = dwSize,
-                    };
+                        String m_FileName = file.Replace(m_Directory, "");
+                        TEntry.dwHash = DatHash.iGetHash(Path.GetFileName(file));
+                    }
+                    else
+                    {
+                        String m_FileName = Path.GetFileNameWithoutExtension(file);
+                        TEntry.dwHash = Convert.ToUInt32(m_FileName, 16);
+                    }
+
+                    //UInt32 dwHash = DatHash.iGetHash(Path.GetFileName(file));
+
+                    TEntry.dwOffset = (UInt32)TDatStream.BaseStream.Position;
+                    TEntry.dwSize = fileData.Length;
+
+                    //var TEntry = new DatEntry
+                    //{
+                    //    dwHash = dwHash,
+                    //    dwOffset = dwOffset,
+                    //    dwSize = dwSize,
+                    //};
 
                     m_EntryTable.Add(TEntry);
                     TDatStream.Write(fileData);
                 }
 
-                TDatStream.Write((UInt32)0); // Write end of entries marker
-
+                TDatStream.Seek(0, SeekOrigin.Begin);
                 foreach (var entry in m_EntryTable)
                 {
                     TDatStream.Write(entry.dwHash);
                     TDatStream.Write(entry.dwOffset);
                     TDatStream.Write(entry.dwSize);
                 }
+
+                TDatStream.Write(0); // Write end of entries marker
+
+                TDatStream.Dispose();
             }
 
             Utils.iSetInfo("[REPACKING]: Completed");
